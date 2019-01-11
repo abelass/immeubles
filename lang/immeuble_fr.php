3052f203b1ce7bfca09345d350bb854d3a219f28
abelass <rainer@websimple.be>
2019-01-11 14:17:36 +0100

    Orthographe

--- lang/immeuble_fr.php
+++ lang/immeuble_fr.php
@@ -21,14 +21,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'icone_creer_immeuble' => 'Créer un immeuble',
-	'icone_modifier_immeuble' => 'Modifier ce immeuble',
+	'icone_modifier_immeuble' => 'Modifier cet immeuble',
 	'info_1_immeuble' => 'Un immeuble',
 	'info_aucun_immeuble' => 'Aucun immeuble',
 	'info_immeubles_auteur' => 'Les immeubles de cet auteur',
 	'info_nb_immeubles' => '@nb@ immeubles',
 
 	// R
-	'retirer_lien_immeuble' => 'Retirer ce immeuble',
+	'retirer_lien_immeuble' => 'Retirer cet immeuble',
 	'retirer_tous_liens_immeubles' => 'Retirer tous les immeubles',
 
 	// S
@@ -36,13 +36,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'texte_ajouter_immeuble' => 'Ajouter un immeuble',
-	'texte_changer_statut_immeuble' => 'Ce immeuble est :',
+	'texte_changer_statut_immeuble' => 'Cet immeuble est :',
 	'texte_creer_associer_immeuble' => 'Créer et associer un immeuble',
-	'texte_definir_comme_traduction_immeuble' => 'Ce immeuble est une traduction du immeuble numéro :',
+	'texte_definir_comme_traduction_immeuble' => 'Cet immeuble est une traduction du immeuble numéro :',
 	'titre_immeuble' => 'Immeuble',
 	'titre_immeubles' => 'Immeubles',
 	'titre_immeubles_rubrique' => 'Immeubles de la rubrique',
-	'titre_langue_immeuble' => 'Langue de ce immeuble',
-	'titre_logo_immeuble' => 'Logo de ce immeuble',
-	'titre_objets_lies_immeuble' => 'Liés à ce immeuble',
+	'titre_langue_immeuble' => 'Langue de cet immeuble',
+	'titre_logo_immeuble' => 'Logo de cet immeuble',
+	'titre_objets_lies_immeuble' => 'Liés à cet immeuble',
 );
